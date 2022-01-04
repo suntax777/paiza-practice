6017397a2a5aefa40f98faf6bc9e2e1504863d04
suntax777 <43720142+suntax777@users.noreply.github.com>
2022-01-04 21:50:40 +0900

    D199-D198

--- src/index.js
+++ src/index.js
@@ -17,3 +17,20 @@ reader.on("close", () => {
   var sum = Number(lines[0]) * Number(lines[1]);
   console.log(sum);
 });
+
+//paizaラーニング　D198:お持ちの個数
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+// 自分の得意な言語で
+// Let's チャレンジ！！
+var lines = [];
+var reader = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on("line", (line) => {
+  lines.push(line);
+});
+reader.on("close", () => {
+  console.log(lines[0] + "/" + lines[1]);
+});
