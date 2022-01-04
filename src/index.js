2bf2519d53042297272e859df9533080a47e0e19
suntax777 <43720142+suntax777@users.noreply.github.com>
2022-01-04 21:53:26 +0900

    D174

--- src/index.js
+++ src/index.js
@@ -34,3 +34,20 @@ reader.on("line", (line) => {
 reader.on("close", () => {
   console.log(lines[0] + "/" + lines[1]);
 });
+
+//D174:栄養の摂取
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+// 自分の得意な言語で
+// Let's チャレンジ！！
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+    var perKiroProtein = 1500
+  console.log(lines[0] * perKiroProtein);
